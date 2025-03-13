c33dc99aef39a718a0294c265af2adfd5e9ff96b
Kushal Doshi <kushalhemant2003@gmail.com>
2025-03-13 07:55:54 +0000

    chore : enhance UI

--- javascript/main.js
+++ javascript/main.js
@@ -1,58 +1,58 @@
 /* 1.) Grab the input value */
 
-document.querySelector(".js-go").addEventListener("click", function(){
+document.querySelector(".js-go").addEventListener("click", function () {
     //alert("testing!!"); by pressing Go! button
 
     //console.log(e); checking e function on mouse click
     var inputValue = document.querySelector(".js-userinput").value;
-        var userInput = getUserInput();
+    var userInput = getUserInput();
     //console.log(input);
     //pushToDOM(input);
-    searchGiphy( userInput );
+    searchGiphy(userInput);
 });
 
-document.querySelector(".js-userinput").addEventListener("keyup", function(e){
+document.querySelector(".js-userinput").addEventListener("keyup", function (e) {
     //alert("testing keyup function!!"); by pressing any key 
     //var input = document.querySelector("input").value;
     //console.log(input);
-    
-    if(e.which === 13){ //'e' is an invisible object or  it is an event listner or a pointer event checker.
+
+    if (e.which === 13) { //'e' is an invisible object or  it is an event listner or a pointer event checker.
         //pushToDOM(input);
         var userInput = getUserInput();
-    searchGiphy( userInput ); 
+        searchGiphy(userInput);
     }
-    
+
 });
 
-function getUserInput(){
+function getUserInput() {
     var inputValue = document.querySelector('.js-userinput').value;
 
     return inputValue;
 }
 
 /* 2.) Do the data stuff with the API */
-function searchGiphy( searchQuery ){
-var url = 'https://api.giphy.com/v1/gifs/search?q='+ searchQuery + '&api_key=4a80JZkB1g5E4N1OkwhqhvUFsY27XcLJ';
+function searchGiphy(searchQuery) {
+    var url = 'https://api.giphy.com/v1/gifs/search?q=' + searchQuery + '&api_key=4a80JZkB1g5E4N1OkwhqhvUFsY27XcLJ';
 
-// AJAX Request
-var GiphyAJAXCall = new XMLHttpRequest();
-GiphyAJAXCall.open( 'GET', url );
-GiphyAJAXCall.send();
+    // AJAX Request
+    var GiphyAJAXCall = new XMLHttpRequest();
+    GiphyAJAXCall.open('GET', url);
+    GiphyAJAXCall.send();
 
 
-GiphyAJAXCall.addEventListener('load', function( data ) {
+    GiphyAJAXCall.addEventListener('load', function (data) {
 
-// your callback events go here 
-var actualData = data.target.response;
-//console.log(data);
-pushToDOM(actualData);
-console.log(actualData);
-});
+        // your callback events go here 
+        var actualData = data.target.response;
+        //console.log(data);
+        pushToDOM(actualData);
+        console.log(actualData);
+    });
 
 }
 
 /* 3.) Show the GIF's */
-function pushToDOM(response){
+function pushToDOM(response) {
     // alert("Checking pushToDOM");
     // turn response into real javascript object
     var response = JSON.parse(response);
@@ -62,14 +62,14 @@ function pushToDOM(response){
     // console.log(imageUrl);
 
     var images = response.data;
-    var container =document.querySelector(".js-container");
+    var container = document.querySelector(".js-container");
     container.innerHTML = "";
 
-    images.forEach(function(image){
+    images.forEach(function (image) {
         var src = image.images.fixed_height.url;
         // console.log(src);
-        
-        container.innerHTML = container.innerHTML + "<img src = \" "+ src +" \"  class = \"container-image\">";
+
+        container.innerHTML = container.innerHTML + "<div class=\"masonry-item\"><img src = \" " + src + " \"  class = \"container-image\"></div>";
     });
 
     // var container =document.querySelector(".js-container");
